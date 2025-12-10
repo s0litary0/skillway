36c764483715e207d94026283d9a522f20f20859
Sultan MacOS <sultanbaiblov1@gmail.com>
2025-12-10 11:24:11 +0500

    footer

--- frontend/src/components/layouts/Footer/Footer.jsx
+++ frontend/src/components/layouts/Footer/Footer.jsx
@@ -1,5 +1,28 @@
+import "./Footer.css";
+
+
 export default function Footer() {
-    return (
-        <>asd</>
-    )
+  return (
+    <div className="footer-container">
+      <div className="left-side">
+        <h4> Skillway </h4>
+        <p className="left-side__description">
+          {" "}
+          Learn, grow, and master new skills.{" "}
+        </p>
+      </div>
+      <div className="right-side">
+        <div className="right-side__fetures-list">
+          <h4>Features</h4>
+          <ul>
+            <li>Courses</li>
+            <li>Profile</li>
+            <li>Dashboard</li>
+            <li>Groups</li>
+            <li>Achievements</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
